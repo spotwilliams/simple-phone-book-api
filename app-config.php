cab948978c97ee4afe61812875423406ea7d8164
Ricardo Vergara <ricardovergarawork@gmail.com>
2020-02-20 12:46:21 -0300

    DI used

--- app-config.php
+++ app-config.php
@@ -1,5 +1,29 @@
 <?php
 
+// Libs
+use Psr\Container\ContainerInterface;
+
+// Business
+use PhoneBook\Repositories\ContactRepository;
+use PhoneBook\Repositories\PersistRepository;
+use PhoneBook\Repositories\PhoneBookRepository;
+
+// Implementations
+use App\Infrastructure\Database\DoctrineContactRepository;
+use App\Infrastructure\Database\DoctrinePersistRepository;
+use App\Infrastructure\Database\DoctrinePhoneBookRepository;
+
 return [
-    \PhoneBook\Repositories\PersistRepository::class => DI\create(\App\Infrastructure\Database\DoctrinePersistRepository::class),
+    'entity-manager' => function (ContainerInterface $container) {
+        return \App\Factories\DoctrineFactory::EntityManager();
+    },
+    PersistRepository::class => function (ContainerInterface $container) {
+        return new DoctrinePersistRepository($container->get('entity-manager'));
+    },
+    ContactRepository::class => function (ContainerInterface $container) {
+        return new DoctrineContactRepository($container->get('entity-manager'));
+    },
+    PhoneBookRepository::class => function (ContainerInterface $container) {
+        return new DoctrinePhoneBookRepository($container->get('entity-manager'));
+    },
 ];
